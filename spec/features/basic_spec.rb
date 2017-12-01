48b756f9ae48cc8976c480fdd7963fa760a15bd9
vramisetti <vramisetti@partner.aligntech.com>
2017-12-01 16:42:25 +0530

    simple space

--- spec/features/basic_spec.rb
+++ spec/features/basic_spec.rb
@@ -4,7 +4,6 @@ describe 'page related tests' , type: :feature do
     feature 'Page operations' do
         scenario 'Create new page' do  
             visit("http://web:3000/")
-            
             click_link("New Product")
             fill_in("product_name",with: "Name")
             save_and_open_screenshot
