a5cc00fd8750a4eba061feb4ae021ae09f1ba215
vramisetti <vramisetti@partner.aligntech.com>
2017-12-04 15:08:09 +0530

    test

--- spec/features/basic_spec.rb
+++ spec/features/basic_spec.rb
@@ -3,7 +3,7 @@ describe 'page related tests' , type: :feature do
 
     feature 'Page operations' do
         scenario 'Create new page' do  
-            visit("http://web:3001/")
+            visit("http://web:3000/")
             click_link("New Product")
             fill_in("product_name",with: "Name")
             save_and_open_screenshot
